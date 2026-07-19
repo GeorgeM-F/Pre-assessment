dfb980c4353eff057d235323b68da9fbbae5c8fb
giorgiomf <giorgiom.fratini@gmail.com>
2026-07-19 15:18:26 +0200

    results.php is now properly functional

--- results.php
+++ results.php
@@ -1,4 +1,14 @@
 <?php
+
+function sommatoria(array $array) {
+  foreach ($array as $value) {
+    if (!is_numeric($value)) {
+      throw new TypeError("L'array contiene valori non numerici.");
+    }
+  }
+  return array_sum($array);
+}
+
 session_start();     // Prova sessione precedente
 try {     // Connessione al database
   $pdo = new PDO("sqlite:database.db");
@@ -9,49 +19,52 @@ try {     // Connessione al database
 
 $stmt = $pdo->query("SELECT risposta FROM risposte_preassessment_prova WHERE risposta = 'no'");     //Array di risposte "no"
 $no = count($stmt->fetchALL());     // Numero di elementi nell'array
-$stmt = $pdo->query("SELECT risposta FROM risposte_preassessment_prova WHERE risposta = 'in parte'");     //Array di risposte "in parte"
-$inparte = count($stmt->fetchALL());     // Numero di elementi nell'array
-$stmt = $pdo->query("SELECT risposta FROM risposte_preassessment_prova WHERE risposta = 'sì'");     //Array di risposte "sì"
-$si = count($stmt->fetchALL());     // Numero di elementi nell'array
+$stmt2 = $pdo->query("SELECT risposta FROM risposte_preassessment_prova WHERE risposta = 'in parte'");     //Array di risposte "in parte"
+$inparte = count($stmt2->fetchALL());     // Numero di elementi nell'array
+$stmt3 = $pdo->query("SELECT risposta FROM risposte_preassessment_prova WHERE risposta = 'sì'");     //Array di risposte "sì"
+$si = count($stmt3->fetchALL());     // Numero di elementi nell'array
 
 // TABELLA DEI VALORI INTERMEDI:
 $vals = array_fill(0, 10, ["criterio_strategie", "criterio_politiche", "criterio_risorse", "criterio_obiettivi", "criterio_metriche"]);
-
-foreach ($vals as $tem) {
-  foreach ($vals as $crit) {
-
+foreach ($vals as $temind => $tem) {
+  foreach ($tem as $critind => $crit) {
 // DALLE DOMANDE SELEZIONA GLI INDICI DI QUELLE CON MACRO-AREA = $tem E $crit = 1
-    $sqli = "SELECT id_domanda FROM domande WHERE risposta = ".$tem." AND ".$crit." = 1";     //query salvata prima come stringa
-    $stmt = $pdo->query($sqli);
-    $indici = $stmt->fetchALL();
+    $sqli4 = "SELECT id_domanda FROM domande WHERE macro_tematica = ".$temind." AND ".$crit." = 1";     //query salvata prima come stringa
+    $stmt4 = $pdo->query($sqli4);
+    $indici = $stmt4->fetchALL(PDO::FETCH_COLUMN, 0);
+    $placehold = implode(',', array_fill(0, count($indici), '?'));
 // DALLE RISPOSTE SELEZIONA GLI AUTOVAL E PRIOR CON QUELL'INDICE
-    $sqli = "SELECT autovalutazione, priorità FROM risposte_preassessment_prova WHERE id_domanda IN ".$indici;
-    $stmt = $pdo->query($sqli);
-    $coppie_valori = $stmt->fetchALL();
-// Ogni valore := media dei valori (prior/autoval)/3*100 delle domande attinenti a tale macro-tema e a tale criterio
-    //$vals[$tem][$crit] = (espressione...)
+    $sqli5 = "SELECT autovalutazione, priorità FROM risposte_preassessment_prova WHERE id_domanda IN ($placehold)";     // AND autovalutazione IS NOT NULL AND priorità IS NOT NULL
+    $stmt5 = $pdo->prepare($sqli5);
+    $stmt5->execute($indici);
+    $coppie_valori = $stmt5->fetchALL();     //aggiungi PDO::FETCH_ASSOC ?
+    $valori_combinati = [];
+    foreach ($coppie_valori as $c) {
+      try {$valori_combinati[] = ($c[1]/$c[0])/3*100;} catch (DivisionByZeroError | TypeError | Error $e) {$valori_combinati[] = "niente";};
+    }
+    try {$vals[$temind][$critind] = sommatoria($valori_combinati)/count($valori_combinati);} catch (DivisionByZeroError | TypeError | Error $e) {$vals[$temind][$critind] = "niente";};
   }
 }
 
-$e1 = array_sum($vals[0])/5;
-$e2 = array_sum($vals[1])/5;
-$e3 = array_sum($vals[2])/5;
-$e4 = array_sum($vals[3])/5;
-$e5 = array_sum($vals[4])/5;
-$s1 = array_sum($vals[5])/5;
-$s2 = array_sum($vals[6])/5;
-$s3 = array_sum($vals[7])/5;
-$s4 = array_sum($vals[8])/5;
-$g1 = array_sum($vals[9])/5;
-$strategie = ($vals[0][0]+$vals[0][1]+$vals[0][2]+$vals[0][3]+$vals[0][4]+$vals[0][5]+$vals[0][6]+$vals[0][7]+$vals[0][8]+$vals[0][9])/10;     // Media di tutti i valori attinenti a tale criterio
-$politiche = 0;     // Media di tutti i valori attinenti a tale criterio
-$risorse = 0;     // Media di tutti i valori attinenti a tale criterio
-$obiettivi = 0;     // Media di tutti i valori attinenti a tale criterio
-$metriche = 0;     // Media di tutti i valori attinenti a tale criterio
-$environmental = ($e1+$e2+$e3+$e4+$e5)/5;
-$social = ($s1+$s2+$s3+$s4)/4;
-$governance = ($g1)/1;
-$complessivo = 0;     // Media di (...)
+try {$e1 = sommatoria($vals[0])/5;} catch (DivisionByZeroError | TypeError | Error $e) {$e1 = "valori non disponibili";};
+try {$e2 = sommatoria($vals[1])/5;} catch (DivisionByZeroError | TypeError | Error $e) {$e2 = "valori non disponibili";};
+try {$e3 = sommatoria($vals[2])/5;} catch (DivisionByZeroError | TypeError | Error $e) {$e3 = "valori non disponibili";};
+try {$e4 = sommatoria($vals[3])/5;} catch (DivisionByZeroError | TypeError | Error $e) {$e4 = "valori non disponibili";};
+try {$e5 = sommatoria($vals[4])/5;} catch (DivisionByZeroError | TypeError | Error $e) {$e5 = "valori non disponibili";};
+try {$s1 = sommatoria($vals[5])/5;} catch (DivisionByZeroError | TypeError | Error $e) {$s1 = "valori non disponibili";};
+try {$s2 = sommatoria($vals[6])/5;} catch (DivisionByZeroError | TypeError | Error $e) {$s2 = "valori non disponibili";};
+try {$s3 = sommatoria($vals[7])/5;} catch (DivisionByZeroError | TypeError | Error $e) {$s3 = "valori non disponibili";};
+try {$s4 = sommatoria($vals[8])/5;} catch (DivisionByZeroError | TypeError | Error $e) {$s4 = "valori non disponibili";};
+try {$g1 = sommatoria($vals[9])/5;} catch (DivisionByZeroError | TypeError | Error $e) {$g1 = "valori non disponibili";};
+try {$strategie = ($vals[0][0]+$vals[0][1]+$vals[0][2]+$vals[0][3]+$vals[0][4]+$vals[0][5]+$vals[0][6]+$vals[0][7]+$vals[0][8]+$vals[0][9])/10;} catch (DivisionByZeroError | TypeError | Error $e) {$strategie = "valori non disponibili";};     // Media di tutti i valori attinenti a tale criterio
+try {$politiche = ($vals[1][0]+$vals[1][1]+$vals[1][2]+$vals[1][3]+$vals[1][4]+$vals[1][5]+$vals[1][6]+$vals[1][7]+$vals[1][8]+$vals[1][9])/10;} catch (DivisionByZeroError | TypeError | Error $e) {$politiche = "valori non disponibili";};     // Media di tutti i valori attinenti a tale criterio
+try {$risorse = ($vals[2][0]+$vals[2][1]+$vals[2][2]+$vals[2][3]+$vals[2][4]+$vals[2][5]+$vals[2][6]+$vals[2][7]+$vals[2][8]+$vals[2][9])/10;} catch (DivisionByZeroError | TypeError | Error $e) {$risorse = "valori non disponibili";};     // Media di tutti i valori attinenti a tale criterio
+try {$obiettivi = ($vals[3][0]+$vals[3][1]+$vals[3][2]+$vals[3][3]+$vals[3][4]+$vals[3][5]+$vals[3][6]+$vals[3][7]+$vals[3][8]+$vals[3][9])/10;} catch (DivisionByZeroError | TypeError | Error $e) {$obiettivi = "valori non disponibili";};     // Media di tutti i valori attinenti a tale criterio
+try {$metriche =  ($vals[4][0]+$vals[4][1]+$vals[4][2]+$vals[4][3]+$vals[4][4]+$vals[4][5]+$vals[4][6]+$vals[4][7]+$vals[4][8]+$vals[4][9])/10;} catch (DivisionByZeroError | TypeError | Error $e) {$metriche = "valori non disponibili";};     // Media di tutti i valori attinenti a tale criterio
+try {$environmental = ($e1+$e2+$e3+$e4+$e5)/5;} catch (DivisionByZeroError | TypeError | Error $e) {$environmental = "valori non disponibili";};
+try {$social = ($s1+$s2+$s3+$s4)/4;} catch (DivisionByZeroError | TypeError | Error $e) {$social = "valori non disponibili";};
+try {$governance = ($g1)/1;} catch (DivisionByZeroError | TypeError | Error $e) {$governance = "valori non disponibili";};
+try {$complessivo = ($environmental+$social+$governance)/3;} catch (DivisionByZeroError | TypeError | Error $e) {$complessivo = "valori non disponibili";};
 ?>
 
 <!DOCTYPE html>
