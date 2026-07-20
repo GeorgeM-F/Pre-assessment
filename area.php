d0623a2f457b2ed85635dc9d4ea2a4c03c2e26c7
giorgiomf <giorgiom.fratini@gmail.com>
2026-07-20 15:05:20 +0200

    start adding the ability to do successive trials

--- area.php
+++ area.php
@@ -6,6 +6,10 @@ try {     // Connessione al database
 } catch (PDOException $e) {
   die("ERRORE! NON E' STATO POSSIBILE CONNETTERSI AL DATABASE." . $e->getMessage());
 }
+
+$linkquer = $pdo->query("SELECT id_prova FROM prove_preassessment");     // Fa una query di tutte le prove presenti nella tabella...
+$linkarr = $linkquer->fetchAll(PDO::FETCH_COLUMN);     // ...e le salva come array.
+$linkarr = [0,1,2,3,4,5,6,7,8,9];     //Cancella questa riga una volta fatto
 ?>
 
 
@@ -28,20 +32,31 @@ try {     // Connessione al database
     <h2>Dati azienda:</h2>
     <hr>
     <?php
-      echo "<p>Ragione sociale: <strong>".$_SESSION["tuonome"]."</strong></p></p>";
-      echo "<p>Partita IVA: <strong>".$_SESSION["tuaiva"]."</strong></p></p>";
-      echo "<p>Codice fiscale: <strong>".$_SESSION["tuocodice"]."</strong></p></p>";
-      echo "<p>Settore: <strong>".$_SESSION["tuosettore"]."</strong></p></p>";
-      echo "<p>Data creazione: <strong>".$_SESSION["tuacreazione"]."</strong></p></p>";
-      echo "<p>Sede: <strong>".$_SESSION["tuasede"]."</strong></p></p>";
-      echo "<p>Codice ATECO: <strong>".$_SESSION["tuoateco"]."</strong></p></p>";
-      echo "<p>Tipo: <strong>".$_SESSION["tuotipo"]."</strong></p></p>";
-      echo "<p>Indirizzo e-mail: <strong>".$_SESSION["tuaemail"]."</strong></p></p>";
+      echo "<p>Ragione sociale: <strong>".$_SESSION["tuonome"]."</strong></p>";
+      echo "<p>Partita IVA: <strong>".$_SESSION["tuaiva"]."</strong></p>";
+      echo "<p>Codice fiscale: <strong>".$_SESSION["tuocodice"]."</strong></p>";
+      echo "<p>Settore: <strong>".$_SESSION["tuosettore"]."</strong></p>";
+      echo "<p>Data creazione: <strong>".$_SESSION["tuacreazione"]."</strong></p>";
+      echo "<p>Sede: <strong>".$_SESSION["tuasede"]."</strong></p>";
+      echo "<p>Codice ATECO: <strong>".$_SESSION["tuoateco"]."</strong></p>";
+      echo "<p>Tipo: <strong>".$_SESSION["tuotipo"]."</strong></p>";
+      echo "<p>Indirizzo e-mail: <strong>".$_SESSION["tuaemail"]."</strong></p>";
     ?>
   </div>
   <div class="sez">
     <h2>Questionari disponibili:</h2>
-    <a class="bot" href="questionnaire.php">PRE-ASSESSMENT</a>
+    <div class="sez">
+      <h2>Pre-assessment</h2>
+      <a class="bot" href="questionnaire.php">Nuova prova</a>
+      <p>Prove precedentemente effettuate:</p>
+      <?php     // REPLICAZIONE LINK PER OGNI ENTRY DELL'ARRAY
+      $n=0;     // Indice per la numerazione dei link
+      foreach ($linkarr as $i) {
+        $n=$n+1;     // Aggiorna l'indice
+        echo '<a class="bot" href="questionnaire.php">Prova n° '.$n.'</a>';
+      }
+      ?>
+    </div>
   </div>
 
   <a class="bot" href="index.php">TORNA INDIETRO</a>
